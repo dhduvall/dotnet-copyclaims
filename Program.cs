ae117e75ca4acd593bcdb39b50e2eb1fce7111b1
Danek Duvall <duvall@coros.net>
2023-09-08 12:20:07 -0700

    initial commit to demonstrate loop in identity

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+using Newtonsoft.Json;
+using Serilog;
+using Serilog.Context;
+using Serilog.Formatting.Json;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console(new JsonFormatter())
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+var jwtParts = new List<string> {
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+    "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ",
+    "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+};
+
+var jwtString = string.Join('.', jwtParts);
+
+var token = new JwtSecurityToken(jwtString);
+var identity = new ClaimsIdentity();
+
+foreach (var claim in token.Claims)
+{
+    if (claim.Type == "iat")
+    {
+        Claim c = new Claim(claim.Type, claim.Value, claim.ValueType, claim.Issuer, claim.OriginalIssuer);
+        identity.AddClaim(c);
+        using (LogContext.PushProperty("Claim", claim, true))
+            Log.Information($"Added claim \"{claim.Type}\"");
+        // identity.AddClaim(claim.Clone());
+    }
+}
+
+using (LogContext.PushProperty("Identity", identity, true))
+    Log.Information("Populated identity with claims");
+
+string json = JsonConvert.SerializeObject(identity);
+System.Console.WriteLine(json);
